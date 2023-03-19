67d65fff8661768ea343f08a9797edf7ce1d4a23
Júlia Cucurella <juliacucurella@gmail.com>
2023-03-19 20:57:21 +0100

    New form to create new tasks

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,55 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/tasks";
+
 export const Form = () => {
+  const navigate = useNavigate();
+
+  const [titleTask, setTitleTask] = useState("");
+  const [descTask, setDescTask] = useState("");
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const newTask = {
+        title: titleTask,
+        desc: descTask,
+        completed: false
+    }
+    axios.post(API_URL, newTask)
+    .then(() => {
+      setTitleTask("")
+      setDescTask("")
+      navigate("/")
+
+    })
+    .catch((err) => {
+      alert('Error connection')
+      console.log(err)
+    })
+  };
+
   return (
-    <form>
-      <label>
-        Enter your pending task:
-        <input type="text" />
-      </label>
-    </form>
+    <>
+      <form onSubmit={handleForm}>
+        <label>
+          Enter your pending task:
+          <input
+            type="text"
+            value={titleTask}
+            onChange={(e) => setTitleTask(e.target.value)}
+            placeholder="Títol de la tasca"
+          />
+          <input
+            type="text"
+            value={descTask}
+            onChange={(e) => setDescTask(e.target.value)}
+            placeholder="Descripció de la tasca"
+          />
+          <input type="submit" />
+        </label>
+      </form>
+    </>
   );
 };
